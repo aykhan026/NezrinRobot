db2902fcb9d692cf903cd9da62444045e2650cbf
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-04-10 00:24:18 +0400

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -158,7 +158,7 @@ dispatcher.add_handler(REMOVE_CHAT_HANDLER)
 dispatcher.add_handler(CHATBOT_HANDLER)
 dispatcher.add_handler(LIST_CB_CHATS_HANDLER)
 
-__mod_name__ = "Chatbot"
+__mod_name__ = "👩🏻‍💼ChatBot"
 __command_list__ = ["addchat", "rmchat", "listaichats"]
 __handlers__ = [
     ADD_CHAT_HANDLER, REMOVE_CHAT_HANDLER, CHATBOT_HANDLER,
