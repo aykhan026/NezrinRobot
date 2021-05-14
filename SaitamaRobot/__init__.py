07eff1aa9082acd821f74ca150b99a896cb4ab7b
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-15 02:23:12 +0400

    Update __init__.py

--- SaitamaRobot/__init__.py
+++ SaitamaRobot/__init__.py
@@ -175,7 +175,6 @@ else:
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
 telethn = TelegramClient("saitama", API_ID, API_HASH)
-pbot = TelegramClient("saitama", API_ID, API_HASH)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
