ac26c80c713df4d6fe8891c051c3ba69eb5356a9
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-22 19:29:12 +0400

    Create tagall.py

---
+++ SaitamaRobot/modules/tagall.py
@@ -0,0 +1,28 @@
+from pyrogram import filters
+
+from YoneRobot.pyrogramee.pluginshelper import admins_only, get_text
+from YoneRobot import pbot
+
+
+@pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
+@admins_only
+async def tagall(client, message):
+    await message.reply("`Processing.....`")
+    sh = get_text(message)
+    if not sh:
+        sh = "Darıxmışam sizinçün 🤭"
+    mentions = ""
+    async for member in client.iter_chat_members(message.chat.id):
+        mentions += member.user.mention + " "
+    n = 4096
+    kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
+    for i in kk:
+        j = f"<b>{sh}</b> \n{i}"
+        await client.send_message(message.chat.id, j, parse_mode="html")
+
+
+__mod_name__ = "🖇️Tağ"
+__help__ = """
+- /tagall : Hərkəsi tağ edər
+- /tagall <mesaj> : Hərkəsi tağ edər və yazdığınız mesaj tağ da görsənər
+"""
