8e85782088af823796b992f3ae93ac8a7b907f23
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-23 03:38:23 +0400

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -4,25 +4,27 @@ from SaitamaRobot.pyrogramee.pluginshelper import admins_only, get_text
 from SaitamaRobot import pbot
 
 
-@pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
+@pbot.on_message(filters.command("tag") & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
     await message.reply("✅`Tağ Prosesi Başladı...`")
     chat_id = message.chat.id
+    string = get_text(message)
+    if not string:
     string = "❤️"
     limit = 1
     icm = client.iter_chat_members(message.chat.id)
     async for member in icm:
         tag = member.user.mention
-        if limit <= 10:
+        if limit <= 40:
             if tag != None:
-                string += f"👤{tag}\n"
+                string += f"{tag}\n"
             else:
                 string += f"{member.user.mention}\n"
             limit += 40
         else:
             await client.send_message(chat_id, text=string)
-            limit = 10
+            limit = 40
             string = ""
             
 
