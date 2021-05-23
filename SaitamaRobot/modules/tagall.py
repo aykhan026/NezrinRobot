c293c9b33ed5f0bb859e552febaaf55e4b7e3c63
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-23 14:04:49 +0400

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -48,23 +48,6 @@ async def tagall(client, message):
             string = ""
             
 
-@pbot.on_message(filters.command("testtag") & ~filters.edited & ~filters.bot)
-@admins_only
-async def tagall(client, message):
-    await message.reply("`Başladıram.....`")
-    sh = get_text(message)
-    if not sh:
-        sh = "Salam!"
-    mentions = ""
-    async for member in client.iter_chat_members(message.chat.id):
-        mentions += member.user.mention + " "
-    if limit <= 1:
-    kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
-    for i in kk:
-        j = f"<b>{sh}</b> \n{i}"
-        await client.send_message(message.chat.id, j)
-       limit = 1
-
 
 __mod_name__ = "🖇️Tağ"
 __help__ = """
