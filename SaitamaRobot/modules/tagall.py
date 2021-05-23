bf8612063a11a6848f45b2a8d67fafa328bc636d
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-24 01:08:20 +0400

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -26,7 +26,7 @@ if not await user_is_admin(
         await event.reply("Yalnız adminlər bu əmri işlədə bilər")
         return
 
-    chat_id = await event.get_input_chat()
+    chat_id = message.chat.id
     string = ""
     limit = 1
     icm = client.iter_chat_members(message.chat.id)
