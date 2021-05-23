8bc40bebf37d9a1548775ddb9e926266c32c856c
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-23 14:56:32 +0400

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -7,7 +7,7 @@ from SaitamaRobot import pbot
 @pbot.on_message(filters.command('all', ['!', '@', '/']) & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
-    await message.reply("🥳 *Qarışıq Tağ Prosesi Başladı...*")
+    await message.reply("🥳 Qarışıq Tağ Prosesi Başladı...")
     chat_id = message.chat.id
     string = ""
     limit = 1
@@ -29,7 +29,7 @@ async def tagall(client, message):
 @pbot.on_message(filters.command('tag', ['!', '@', '/']) & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
-    await message.reply("🥳 *Tək-Tək Tağ Prosesi Başladı...*")
+    await message.reply("🥳 Tək-Tək Tağ Prosesi Başladı...")
     chat_id = message.chat.id
     string = ""
     limit = 1
