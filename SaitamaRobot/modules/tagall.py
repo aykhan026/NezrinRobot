64b464ec682f19df7f71bb80a9b0156243c19f2e
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-26 00:44:51 +0400

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -58,11 +58,11 @@ async def tagall(client, message):
     mentions = ""
     async for member in client.iter_chat_members(message.chat.id):
         mentions += member.user.mention + " "
-    n = 4096
+    n = 800
     kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
     for i in kk:
-        j = f"<b>{sh}</b> \n{i}"\n
-        await client.send_message(message.chat.id, j, parse_mode="html")
+        j = f"<b>{sh}</b> \n{i}"
+        await client.send_message(message.chat.id, j)
 
 
 
