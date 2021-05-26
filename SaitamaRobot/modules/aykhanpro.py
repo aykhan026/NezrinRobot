d2c4a663c294a3d383aeb2d47103f0750e49da15
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-26 22:19:05 +0400

    Create aykhanpro.py

---
+++ SaitamaRobot/modules/aykhanpro.py
@@ -0,0 +1,6 @@
+__mod_name__ = "😈Aykhan Pro"
+__help__ = """
+✅ *Yalnız adminlər* tərəfindən istifadə oluna bilər !
+✅ Bu əmrlər *[@ . / !]* ilə işlədilir
+- `@all` : Son görülməsi yaxın olan hərkəsi qarışıq tağ edər
+- `@tag` : Son görülməsi yaxın olan hər kəsi tək tək tağ edər
