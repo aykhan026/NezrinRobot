8a47d2284212e3f5762be2d8735a0742ee65f190
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-05-26 22:36:26 +0400

    Update aykhanpro.py

--- SaitamaRobot/modules/aykhanpro.py
+++ SaitamaRobot/modules/aykhanpro.py
@@ -1,8 +1,19 @@
 __mod_name__ = "😈Aykhan Pro"
 __help__ = """
+
+🖇️*Tağ modulu*
 ✅ *Yalnız adminlər* tərəfindən istifadə oluna bilər !
 ✅ Bu əmrlər *[@ . / !]* ilə işlədilir
 - `@all` : Son görülməsi yaxın olan hərkəsi qarışıq tağ edər
 - `@tag` : Son görülməsi yaxın olan hər kəsi tək tək tağ edər
 
+🧸*Stiker modulu*
+- `/q` : Yanıtladığınız mesajı stiker edəcək
+- `/q reng` : yanıtladığınız mesajı müxtəlif rənglərdə stiker edəcək
+
+🗣️*Səs modulu*
+- `/tts <mesaj>` : Yazdığınız mesajı səsə çevirəcək
+
+🎧*Musiqi modulu*
+ - `/song <musiqi adı>` : Musiqini YouTubedən yükləyəcəm
 """
