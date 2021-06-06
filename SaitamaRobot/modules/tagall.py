dfec7c40c647df5432a74791d776d1d0ff33ac2e
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-06-06 18:17:18 +0400

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -51,24 +51,29 @@ async def tag(client, message):
             await client.send_message(chat_id, text=string)
             limit = 1
             string = ""
+
+@pbot.on_message(filters.command('tagstop', ['!', '/', '.']) & ~filters.edited & ~filters.bot)
+async def tagstop(client, message):
+    await message.reply("😌 Tağ etməə prosesi dayandırıldı")
+    chat_id = message.chat.id
+    string = get_text(message)
+    if not string:
+        string = ""
+    limit = 1
+    icm = client.iter_chat_members(message.chat.id)
+    async for member in icm:
+        tag = member.user.mention
+        if limit <= 1:
+            if tag != None:
+                string += f"❤️ {tag} Bayaqdan səni gözləyirəm gəl qrupa 🥰 \n"
+            else:
+                string += f"{member.user.mention}\n"
+            limit += 1
+        else:
+            await client.send_message(chat_id, text=string)
+            limit = 1
+            string = ""
             
-@pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
-@admins_only
-async def tagall(client, message):
-    await message.reply("`Processing.....`")
-    sh = get_text(message)
-    if not sh:
-        sh = "Hi!"
-    mentions = ""
-    async for member in client.iter_chat_members(message.chat.id):
-        mentions += member.user.mention + " "
-    n = 4096
-    kk = [mentions[i:i+n] for i in range(0, len(mentions), n)]
-    for i in kk:
-        j = f"<b>{sh}</b> \n{i}"
-        await client.send_message(message.chat.id, j, parse_mode="html")
-
-
 
 
 __mod_name__ = "🖇️Tağ"
