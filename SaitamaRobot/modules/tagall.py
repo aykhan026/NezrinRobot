ef1aa5d4a0927d7ccf51c3a628aeb199063d89a7
Aykhan <78858146+aykhan026@users.noreply.github.com>
2021-06-06 18:20:00 +0400

    Update tagall.py

--- SaitamaRobot/modules/tagall.py
+++ SaitamaRobot/modules/tagall.py
@@ -81,5 +81,6 @@ __help__ = """
 ✅ *Yalnız adminlər* tərəfindən istifadə oluna bilər !
 ✅ Bu əmrlər *[@ . / !]* ilə işlədilir
 - `@all` : Son görülməsi yaxın olan hərkəsi qarışıq tağ edər
-- `@tag` : Son görülməsi yaxın olan hər kəsi tək tək tağ edər
+- `.tag` : Son görülməsi yaxın olan hər kəsi tək tək tağ edər
+- `!tagstop` Aktiv tağ etmə prosesini dayandırır
 """
